97da44c7d82c084db80ed8a12be5b132c81e1c6f
Saqib Amin <saqibamin257@gmail.com>
2020-06-23 07:47:11 +0500

    03-pass more than one props

--- 01-react-basics/src/App.js
+++ 01-react-basics/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App(props) {
   return (
-    <h1>Hi {props.name}</h1>
+  <h1>Hi {props.firstName} {props.lastName}!</h1>
   );
 }
 
--- 01-react-basics/src/index.js
+++ 01-react-basics/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={"saqib amin"} />
+    <App firstName={"saqib"} lastName={"amin"} />
   </React.StrictMode>,
   document.getElementById('root')
 );
