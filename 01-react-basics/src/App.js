8b9ea70ab6efb340acacbd86cb628d1353f51eda
Saqib Amin <saqibamin257@gmail.com>
2020-06-23 08:08:10 +0500

    05-pass props by ES6 constant and arrow function

--- 01-react-basics/src/App.js
+++ 01-react-basics/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App({firstName,lastName}) {
-  return (
-  <h1>Hi {firstName} {lastName}!</h1>
-  );
+// function App({firstName,lastName}) {
+//   return (
+//   <h1>Hi {firstName} {lastName}!</h1>
+//   );
+// }
+
+const App = ({firstName,lastName}) =>{
+return <h1>Hello {firstName} {lastName}!</h1>
 }
 
 export default App;
