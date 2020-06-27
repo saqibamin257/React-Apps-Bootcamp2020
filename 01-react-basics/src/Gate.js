cbdf1047c06c70a964cdee59741b01c031588439
Saqib Amin <saqibamin257@gmail.com>
2020-06-27 06:35:31 +0500

    07-StateExercise create media component with object as props & create Gate component with ternary operation logic (? and :)

---
+++ 01-react-basics/src/Gate.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Gate = ({isOpen}) => (
+<div>{isOpen?"Open":"Closed"}</div>
+)
+
+export default Gate;
--- 01-react-basics/src/index.js
+++ 01-react-basics/src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MediaCard from './MediaCard';
+import Gate from './Gate';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App firstName={"saqib"} lastName={"amin"} />
+
+
+
+    <MediaCard media={{
+                       title:"Great China Wall!", 
+                       body:"This is wonder of the world!",
+                       imageUrl:"https://image.cnbcfm.com/api/v1/image/102013848-china-great-wall.jpg?v=1532564419&w=678&h=381" 
+                      }}
+                      />
+                      
+  <Gate isOpen={false}/>
+  
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
